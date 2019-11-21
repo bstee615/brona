1600d94d75d8bd9021e1e2f2fbdbf31ace065a7c
Ben Steenhoek <benjis@iastate.edu>
2019-11-21 02:44:40 -0600

    Standardize sprite positions to tile indices

--- brona.js
+++ brona.js
@@ -10,7 +10,7 @@ const tilesize = canvas.width / 24;
 let bronaSprite = new Image();
 let bronaSpriteLoaded = false;
 let x = 50, y = 50;
-let w = tilesize, h = tilesize;
+let w = 1, h = 1;
 bronaSprite.onload = function() {
   bronaSpriteLoaded = true;
 };
@@ -88,7 +88,7 @@ for (let row = 0; row < 12; row ++) {
     }
 }
 
-function loadTile(row, col, name = "Unnamed", x = 0, y = 0, w = tilesize, h = tilesize) {
+function loadTile(row, col, name = "Unnamed", x = 0, y = 0, w = 1, h = 1) {
     let tile = tileInfo[row][col];
     if (tile.loaded) {
         return;
@@ -102,23 +102,23 @@ function loadTile(row, col, name = "Unnamed", x = 0, y = 0, w = tilesize, h = ti
         tile.name = name;
         tile.x = x;
         tile.y = y;
-        tile.w = tilesize;
-        tile.h = tilesize;
+        tile.w = w;
+        tile.h = h;
         tile.srcW = tilewidth;
         tile.srcH = tilewidth;
         tile.srcX = col * tilewidth;
         tile.srcY = row * tilewidth;
     };
     tile.img.src = imagePath("forest_tiles.png");
-    
+
     tileInfo[row][col] = tile;
 }
 
 loadTile(0, 13, "Mushroom");
-loadTile(6, 0, "Pine_1", tilesize * 4, tilesize * 4);
-loadTile(7, 0, "Pine_2", tilesize * 4, tilesize * 5);
-loadTile(6, 1, "Pine_3", tilesize * 5, tilesize * 4);
-loadTile(7, 1, "Pine_4", tilesize * 5, tilesize * 5);
+loadTile(6, 0, "Pine_1", 2, 4);
+loadTile(7, 0, "Pine_2", 2, 5);
+loadTile(6, 1, "Pine_3", 3, 4);
+loadTile(7, 1, "Pine_4", 3, 5);
 
 function renderLoopStep() {
     // Draw background image
@@ -128,14 +128,14 @@ function renderLoopStep() {
 
     // Draw Brona
     if (bronaSpriteLoaded) {
-        ctx.drawImage(bronaSprite, x, y, w, h);
+        ctx.drawImage(bronaSprite, x, y, w * tilesize, h * tilesize);
     }
 
     // Draw loaded tiles
     for (const row of tileInfo) {
         for (const tile of row) {
             if (tile.loaded) {
-                ctx.drawImage(tile.img, tile.srcX, tile.srcY, tile.srcW, tile.srcH, tile.x, tile.y, tile.w, tile.h);
+                ctx.drawImage(tile.img, tile.srcX, tile.srcY, tile.srcW, tile.srcH, tile.x * tilesize, tile.y * tilesize, tile.w * tilesize, tile.h * tilesize);
             }
         }
     }
