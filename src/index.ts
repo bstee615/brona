0ec2bbd6007c25fabd20d977251acf3a8754210f
Ben Steenhoek <benjis@iastate.edu>
2019-11-22 15:40:54 -0600

    Split tile map into object

--- src/index.ts
+++ src/index.ts
@@ -2,27 +2,29 @@ import * as rendering from "./rendering";
 
 import * as brona from "./brona";
 
-import * as tiles from "./tiles";
+import {Tilemap} from "./tiles";
 import Sprite from "./sprite";
 import { canvas } from "./rendering";
 import { mousedown, mousePosition, rightMousedown } from "./control";
 
-tiles.loadTile(0, 13, "Mushroom");
-tiles.loadTile(0, 13, "Mushroom", 0, 1);
-tiles.loadTile(6, 0, "Pine_1", 2, 4);
-tiles.loadTile(7, 0, "Pine_2", 2, 5);
-tiles.loadTile(6, 1, "Pine_3", 3, 4);
-tiles.loadTile(7, 1, "Pine_4", 3, 5);
+let forestTiles = new Tilemap("forest_tiles.png", 12, 16, 32);
 
-tiles.loadObject(0, 6, "Pit_1_1", 5, 5, 1, 1, true);
-tiles.loadObject(0, 7, "Pit_2_1", 6, 5, 1, 1, true);
-tiles.loadObject(0, 8, "Pit_3_1", 7, 5, 1, 1, true);
-tiles.loadObject(1, 6, "Pit_1_2", 5, 6, 1, 1, true);
-tiles.loadObject(1, 7, "Pit_2_2", 6, 6, 1, 1, true);
-tiles.loadObject(1, 8, "Pit_3_2", 7, 6, 1, 1, true);
-tiles.loadObject(2, 6, "Pit_1_3", 5, 7, 1, 1, true);
-tiles.loadObject(2, 7, "Pit_2_3", 6, 7, 1, 1, true);
-tiles.loadObject(2, 8, "Pit_3_3", 7, 7, 1, 1, true);
+forestTiles.loadTile(0, 13, "Mushroom");
+forestTiles.loadTile(0, 13, "Mushroom", 0, 1);
+forestTiles.loadTile(6, 0, "Pine_1", 2, 4);
+forestTiles.loadTile(7, 0, "Pine_2", 2, 5);
+forestTiles.loadTile(6, 1, "Pine_3", 3, 4);
+forestTiles.loadTile(7, 1, "Pine_4", 3, 5);
+
+forestTiles.loadObject(0, 6, "Pit_1_1", 5, 5, 1, 1, true);
+forestTiles.loadObject(0, 7, "Pit_2_1", 6, 5, 1, 1, true);
+forestTiles.loadObject(0, 8, "Pit_3_1", 7, 5, 1, 1, true);
+forestTiles.loadObject(1, 6, "Pit_1_2", 5, 6, 1, 1, true);
+forestTiles.loadObject(1, 7, "Pit_2_2", 6, 6, 1, 1, true);
+forestTiles.loadObject(1, 8, "Pit_3_2", 7, 6, 1, 1, true);
+forestTiles.loadObject(2, 6, "Pit_1_3", 5, 7, 1, 1, true);
+forestTiles.loadObject(2, 7, "Pit_2_3", 6, 7, 1, 1, true);
+forestTiles.loadObject(2, 8, "Pit_3_3", 7, 7, 1, 1, true);
 
 const bg = new Sprite("forest.png");
 
@@ -34,7 +36,7 @@ function renderLoopStep() {
     bg.draw(rendering.ctx, 0, 0, rendering.canvas.clientWidth, rendering.canvas.clientHeight);
 
     // Draw loaded tiles
-    for (const tile of tiles.tiles) {
+    for (const tile of forestTiles.tiles) {
         tile.draw(rendering.ctx);
     }
 
--- src/tiles.ts
+++ src/tiles.ts
@@ -3,74 +3,93 @@ import Sprite from "./sprite";
 import {registerCollider} from "./colliders";
 
 // Tile stuff
-
-// 12x16 tiles in forest_tiles.png
-let tileSprites = [];
-for (let row = 0; row < 12; row ++) {
-    tileSprites.push([]);
-    for (let col = 0; col < 16; col ++) {
-        tileSprites[row].push(new Sprite());
-    }
-}
-
-// Holds instances of the tiles
-export let tiles = [];
-
-function loadSprite(row: number, col: number) {
-    if (tileSprites[row][col].loaded) {
-        return;
-    }
-
-    const tilewidth = 32;
-
-    tileSprites[row][col] = new Sprite("forest_tiles.png", 
-    col * tilewidth, row * tilewidth,
-    tilewidth, tilewidth,
-        );
-}
-
-export function loadTile(tilerow: number, tilecol: number, name = "Unnamed", x = 0, y = 0, w = 1, h = 1) {
-    loadSprite(tilerow, tilecol);
-
-    const tile = new TileObject(x, y, w, h, 
-        tilerow,
-        tilecol,
-        name);
-
-    tiles.push(tile);
-
-    return tile;
-}
-
-export function loadObject(tilerow: number, tilecol: number, name = "Unnamed", x = 0, y = 0, w = 1, h = 1, collider=false) {
-    const tile = loadTile(tilerow, tilecol, name, x, y, w, h);
-
-    if (collider) {
-        registerCollider(tile);
-    }
-}
-
+    
 class TileObject extends GameObject {
+    map: Tilemap;
+
     tilerow: number;
     tilecol: number;
 
     name: string;
 
-    constructor(x: number, y: number, w: number, h: number, tilerow: number, tilecol: number, name: string) {
+    constructor(map: Tilemap, x: number, y: number, w: number, h: number, tilerow: number, tilecol: number, name: string) {
         super(x, y, w, h);
 
         this.tilerow = tilerow;
         this.tilecol = tilecol;
 
         this.name = name;
+
+        this.map = map;
     }
 
     sprite() {
-        loadSprite(this.tilerow, this.tilecol);
-        return tileSprites[this.tilerow][this.tilecol];
+        this.map.loadSprite(this.tilerow, this.tilecol);
+        return this.map.tileSprites[this.tilerow][this.tilecol];
     }
 }
 
-// export function drawSprite(ctx, tile) {
-//     tile.getSprite().draw(ctx, gameToPx(tile.x), gameToPx(tile.y), gameToPx(tile.w), gameToPx(tile.h));
-// }
+export class Tilemap {
+    filepath: string;
+
+    // Holds instances of the tiles
+    tiles: Array<TileObject>;
+
+    // 12x16 tiles in forest_tiles.png
+    tileSprites: Array<Array<Sprite>>;
+    
+    nrows: number;
+    ncols: number;
+    tilewidth: number;
+
+    constructor(filepath: string, nrows: number, ncols: number, tilewidth: number) {
+        this.filepath = filepath;
+
+        this.tileSprites = [];
+        for (let row = 0; row < nrows; row ++) {
+            this.tileSprites.push([]);
+            for (let col = 0; col < ncols; col ++) {
+                this.tileSprites[row].push(new Sprite());
+            }
+        }
+        
+        this.tiles = [];
+
+        this.tilewidth = tilewidth;
+        this.nrows = nrows;
+        this.ncols = ncols;
+    }
+
+    loadSprite(row: number, col: number): Sprite {
+        if (this.tileSprites[row][col].loaded) {
+            return this.tileSprites[row][col];
+        }
+    
+        return new Sprite(this.filepath, 
+        col * this.tilewidth, row * this.tilewidth,
+        this.tilewidth, this.tilewidth,
+            );
+    }
+    
+    loadTile(tilerow: number, tilecol: number, name = "Unnamed", x = 0, y = 0, w = 1, h = 1): TileObject {
+        this.tileSprites[tilerow][tilecol] = this.loadSprite(tilerow, tilecol);
+    
+        const tile = new TileObject(this,
+            x, y, w, h, 
+            tilerow,
+            tilecol,
+            name);
+    
+        this.tiles.push(tile);
+    
+        return tile;
+    }
+    
+    loadObject(tilerow: number, tilecol: number, name = "Unnamed", x = 0, y = 0, w = 1, h = 1, collider=false) {
+        const tile = this.loadTile(tilerow, tilecol, name, x, y, w, h);
+    
+        if (collider) {
+            registerCollider(tile);
+        }
+    }
+}
